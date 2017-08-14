d088e77cd71c57c09b1b211a6ce44aea0f74a242
Jonathan Chang <j.a.chang820@gmail.com>
2017-08-14 04:05:21 -0700

    Setup cucumber

---
+++ features/step_definitions/hangman_steps.rb
@@ -0,0 +1,35 @@
+
+
+When /^I try to go to the URL "(.*)"$/ do |url|
+  visit url
+end
+
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
+
+Then /^(?:|I )should see "([^\"]*)"(?: within "([^\"]*)")?$/ do |text, selector|
+  with_scope(selector) do
+    if page.respond_to? :should
+      page.should have_content(text)
+    else
+      assert page.has_content?(text)
+    end
+  end
+end
+
+When /^(?:|I )press "([^\"]*)"(?: within "([^\"]*)")?$/ do |button, selector|
+  with_scope(selector) do
+    click_button(button)
+  end
+end
+
